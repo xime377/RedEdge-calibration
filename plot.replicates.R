b509e44648a3965138456498b34fdcb815aa2260
Ximena Tagle Casapía <xtagle@lamolina.edu.pe>
2017-04-17 13:01:35 +0200

    Functions

--- plot.replicates.R
+++ plot.replicates.R
@@ -91,7 +91,7 @@ plot.repR <- function(my_data)
     stat_summary(fun.y = "mean", colour = "black", size = 1, geom = "line")+
     scale_x_continuous(breaks = seq(465, 865, by = 20), 
                        labels = insert_minor(seq(465, 865, by=100), 4), 
-                       limits = c(465, 860), expand = c(0, 0)) +
+                       limits = c(465, 865), expand = c(0, 0)) +
     scale_y_continuous(breaks = seq(0, 1, by = 0.1), expand = c(0, -0.05), limits = c(-0.05, 1.05))+            #Set max 1
     theme(axis.text=element_text(size=12),
           axis.title=element_text(size=14,face="bold"))+
@@ -123,9 +123,9 @@ plot.graySM<-function(my_data)
   g<-ggplot(my_data, aes(x=Wavelength, y=Reflectance, colour=Target)) + 
     geom_line(size=0.8)+
     scale_colour_grey(name="")+
-    scale_x_continuous(breaks= seq(325,1080,by=30), 
-                       labels = insert_minor(seq(325, 1080, by=150),4), 
-                       limits = c(325,1080), expand =c(0,0)) +
+    scale_x_continuous(breaks = seq(465, 865, by = 20), 
+                       labels = insert_minor(seq(465, 865, by=100), 4), 
+                       limits = c(465, 865), expand = c(0, 0)) +
     scale_y_continuous(expand =c(0,-0.05), limits= c(-0.05, 1.05))+            #Set max 1
     theme(axis.text=element_text(size=12),
           axis.title=element_text(size=14,face="bold"))+
