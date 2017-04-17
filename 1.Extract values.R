7a82559983a8b486b7a39971f109c837474dae53
Ximena Tagle Casapía <xtagle@lamolina.edu.pe>
2017-04-17 22:12:04 +0200

    Analysis
    Values extraction

---
+++ 1.Extract values.R
@@ -0,0 +1,221 @@
+#Set WD
+#setwd("E:/Ximena/MSc Thesis/Results")
+setwd("C:/Users/Ximena/Google Drive/MSc Thesis/Results")
+
+###Load libraries
+#library(pacman)
+#pacman::p_load(Rmisc) To install several packages at the same time
+library(reshape2)
+library(raster)
+library(maptools)
+library(stringr)
+library(Rmisc)
+library(mapview)
+
+#Load imgs
+dir(,"tif")
+
+T.img.path23 <- lapply(list.files(path=("./Calibration/Cal_pics/2017_03_13/"), (pattern="0010.*.tif$"), 
+                                  full.names=T), raster) #9,23,44
+
+T.img.path23 <- lapply(list.files(path=("./Calibration/Cal_pics/2017_03_13/"), (pattern="0011.*.tif$"), 
+                                  full.names=T), raster) #9,23,44
+
+T.img.path23 <- lapply(list.files(path=("./Calibration/Cal_pics/2017_03_13/"), (pattern="0012.*.tif$"), 
+                                  full.names=T), raster) #9,23,44
+
+T.img.path23 <- lapply(list.files(path=("./Calibration/Cal_pics/2017_03_13/"), (pattern="0010.*.tif$"), 
+                                  full.names=T), raster) #9,23,44
+
+T.img.path23 <- lapply(list.files(path=("./Calibration/Cal_pics/2017_03_13/"), (pattern="0062.*.tif$"), 
+                                  full.names=T), raster) #9,23,44
+
+T.img.path23 <- lapply(list.files(path=("./Calibration/Cal_pics/2017_03_13/"), (pattern="0063.*.tif$"), 
+                                  full.names=T), raster) #9,23,44
+
+T.img.path23 <- lapply(list.files(path=("./Calibration/Cal_pics/2017_03_13/"), (pattern="0064.*.tif$"), 
+                                  full.names=T), raster) #9,23,44
+
+T.img.path23 <- lapply(list.files(path=("./Calibration/Cal_pics/2017_03_24/1/"), (pattern="0008.*.tif$"), 
+                                  full.names=T), raster) #9%
+
+T.img.path23 <- lapply(list.files(path=("./Calibration/Cal_pics/2017_03_24/2/"), (pattern="0004.*.tif$"), 
+                                  full.names=T), raster) #9
+
+T.img.path23 <- lapply(list.files(path=("./Calibration/Cal_pics/2017_04_08/1/"), (pattern="0145.*.tif$"), 
+                                  full.names=T), raster) #9%
+
+T.img.path23 <- lapply(list.files(path=("./Calibration/Cal_pics/2017_04_08/2/"), (pattern="0147.*.tif$"), 
+                                  full.names=T), raster) #9%
+
+T.img.path23 <- lapply(list.files(path=("./Calibration/Cal_pics/2017_04_08/2/"), (pattern="0148.*.tif$"), 
+                                  full.names=T), raster) #9%
+
+T.img.path23 <- lapply(list.files(path=("./Calibration/Cal_pics/2017_03_24/1/"), (pattern="0009.*.tif$"), 
+                                  full.names=T), raster) #23%
+
+
+
+
+
+T.img.path1 <- lapply(list.files(path=("./Calibration/Cal_pics/2016_08_12/"), (pattern="0001.*.tif$"), 
+                                full.names=T), raster) #RP02-...282 ->50%
+
+T.img.path2 <- lapply(list.files(path=("./Calibration/Cal_pics/2016_08_12/"), (pattern="0116.*.tif$"), 
+                                 full.names=T), raster) #RP02-...282 ->50%
+
+T.img.path3 <- lapply(list.files(path=("./Calibration/Cal_pics/2016_08_12/"), (pattern="0122.*.tif$"), 
+                                 full.names=T), raster) #RP02-...282 ->50%
+
+T.img.path13 <- lapply(list.files(path=("./Calibration/Cal_pics/2017_03_07/1/after/"), (pattern="0000.*.tif$"), 
+                                  full.names=T), raster) #RP02-...282 ->50%
+
+T.img.path4 <- lapply(list.files(path=("./Calibration/Cal_pics/2016_08_12/"), (pattern="0123.*.tif$"), 
+                                 full.names=T), raster) #RP02-...200 ->80%
+
+T.img.path5 <- lapply(list.files(path=("./Calibration/Cal_pics/2016_11_03/1/"), (pattern="0000.*.tif$"), 
+                                 full.names=T), raster) #RP02-...200 ->80%
+
+T.img.path6 <- lapply(list.files(path=("./Calibration/Cal_pics/2016_11_03/1/"), (pattern="0001.*.tif$"), 
+                                 full.names=T), raster) #RP02-...200 ->80%
+
+T.img.path7 <- lapply(list.files(path=("./Calibration/Cal_pics/2016_11_03/1/"), (pattern="0004.*.tif$"), 
+                                 full.names=T), raster) #RP02-...200 ->80%
+
+T.img.path8 <- lapply(list.files(path=("./Calibration/Cal_pics/2016_11_03/1/"), (pattern="0005.*.tif$"), 
+                                 full.names=T), raster) #RP02-...200 ->80%
+
+T.img.path9 <- lapply(list.files(path=("./Calibration/Cal_pics/2016_11_03/2/"), (pattern="0000.*.tif$"), 
+                                 full.names=T), raster) #RP02-...200 ->80%
+
+T.img.path10 <- lapply(list.files(path=("./Calibration/Cal_pics/2016_11_03/2/"), (pattern="0002.*.tif$"), 
+                                 full.names=T), raster) #RP02-...200 ->80%
+
+T.img.path11 <- lapply(list.files(path=("./Calibration/Cal_pics/2016_12_02/"), (pattern="0000.*.tif$"), 
+                                  full.names=T), raster) #RP02-...200 ->80%
+
+T.img.path12 <- lapply(list.files(path=("./Calibration/Cal_pics/2016_12_02/"), (pattern="0002.*.tif$"), 
+                                  full.names=T), raster) #RP02-...200 ->80%
+
+
+
+T.img.path14 <- lapply(list.files(path=("./Calibration/Cal_pics/2017_03_07/1/after/"), (pattern="0001.*.tif$"), 
+                                  full.names=T), raster) #RP02-...200 ->80%
+
+T.img.path15 <- lapply(list.files(path=("./Calibration/Cal_pics/2017_03_07/1/before/"), (pattern="0000.*.tif$"), 
+                                  full.names=T), raster) #RP02-...282 ->50%
+
+T.img.path16 <- lapply(list.files(path=("./Calibration/Cal_pics/2017_03_07/1/before/"), (pattern="0001.*.tif$"), 
+                                  full.names=T), raster) #RP02-...200 ->80%
+
+T.img.path17 <- lapply(list.files(path=("./Calibration/Cal_pics/2017_03_07/2/"), (pattern="0000.*.tif$"), 
+                                  full.names=T), raster) #RP02-...200 ->80%
+
+T.img.path18 <- lapply(list.files(path=("./Calibration/Cal_pics/2017_03_07/2/"), (pattern="0001.*.tif$"), 
+                                  full.names=T), raster) #RP02-...200 ->80%
+
+T.img.path19 <- lapply(list.files(path=("./Calibration/Cal_pics/2017_03_07/2/"), (pattern="0002.*.tif$"), 
+                                  full.names=T), raster) #RP02-...282 ->50%
+
+T.img.path20 <- lapply(list.files(path=("./Calibration/Cal_pics/2017_03_07/2/"), (pattern="0003.*.tif$"), 
+                                  full.names=T), raster) #RP02-...282 ->50%
+
+T.img.path21 <- lapply(list.files(path=("./Calibration/Cal_pics/2017_03_07/2/"), (pattern="0005.*.tif$"), 
+                                  full.names=T), raster) #RP02-...200 ->80%
+
+T.img.path21 <- lapply(list.files(path=("./Calibration/Cal_pics/2017_03_07/2/"), (pattern="0006.*.tif$"), 
+                                  full.names=T), raster) #RP02-...200 ->80%
+
+T.img.path22 <- lapply(list.files(path=("./Calibration/Cal_pics/2017_03_07/2/"), (pattern="0007.*.tif$"), 
+                                  full.names=T), raster) #RP02-...282 ->50%
+
+T.img.path23 <- lapply(list.files(path=("./Calibration/Cal_pics/2017_03_07/2/"), (pattern="0008.*.tif$"), 
+                                  full.names=T), raster) #RP02-...282 ->50%
+
+T.img.path23 <- lapply(list.files(path=("./Calibration/Cal_pics/2017_03_24/1/"), (pattern="0000.*.tif$"), 
+                                  full.names=T), raster) #RP02-...282 ->50%
+
+T.img.path23 <- lapply(list.files(path=("./Calibration/Cal_pics/2017_03_24/1/"), (pattern="0001.*.tif$"), 
+                                  full.names=T), raster) #RP02-...200 ->80%
+
+T.img.path23 <- lapply(list.files(path=("./Calibration/Cal_pics/2017_03_24/1/"), (pattern="0006.*.tif$"), 
+                                  full.names=T), raster) #RP02-...200 ->80%
+
+T.img.path23 <- lapply(list.files(path=("./Calibration/Cal_pics/2017_03_24/1/"), (pattern="0007.*.tif$"), 
+                                  full.names=T), raster) #44%
+
+T.img.path23 <- lapply(list.files(path=("./Calibration/Cal_pics/2017_03_24/2/"), (pattern="0000.*.tif$"), 
+                                  full.names=T), raster) #RP02-...200 ->80%
+
+T.img.path23 <- lapply(list.files(path=("./Calibration/Cal_pics/2017_03_24/2/"), (pattern="0000.*.tif$"), 
+                                  full.names=T), raster) #RP02-...200 ->80%
+
+T.img.path23 <- lapply(list.files(path=("./Calibration/Cal_pics/2017_03_24/2/"), (pattern="0001.*.tif$"), 
+                                  full.names=T), raster) #RP02-...282 ->50%
+
+T.img.path23 <- lapply(list.files(path=("./Calibration/Cal_pics/2017_03_24/2/"), (pattern="0002.*.tif$"), 
+                                  full.names=T), raster) #RP02-...282 ->50%
+
+T.img.path23 <- lapply(list.files(path=("./Calibration/Cal_pics/2017_03_24/2/"), (pattern="0003.*.tif$"), 
+                                  full.names=T), raster) #44
+
+T.img.path23 <- lapply(list.files(path=("./Calibration/Cal_pics/2017_03_24/2/"), (pattern="0005.*.tif$"), 
+                                  full.names=T), raster) #23
+
+T.img.path23 <- lapply(list.files(path=("./Calibration/Cal_pics/2017_04_08/1/"), (pattern="0141.*.tif$"), 
+                                  full.names=T), raster) #RP02-...282 ->50%
+
+T.img.path23 <- lapply(list.files(path=("./Calibration/Cal_pics/2017_04_08/1/"), (pattern="0142.*.tif$"), 
+                                  full.names=T), raster) #RP02-...282 ->50%
+
+T.img.path23 <- lapply(list.files(path=("./Calibration/Cal_pics/2017_04_08/1/"), (pattern="0143.*.tif$"), 
+                                  full.names=T), raster) #RP02-...200 ->80%
+
+T.img.path23 <- lapply(list.files(path=("./Calibration/Cal_pics/2017_04_08/1/"), (pattern="0144.*.tif$"), 
+                                  full.names=T), raster) #RP02-...200 ->80%
+
+T.img.path23 <- lapply(list.files(path=("./Calibration/Cal_pics/2017_04_08/1/"), (pattern="0146.*.tif$"), 
+                                  full.names=T), raster) #23%
+
+T.img.path23 <- lapply(list.files(path=("./Calibration/Cal_pics/2017_04_08/1/"), (pattern="0147.*.tif$"), 
+                                  full.names=T), raster) #44%
+
+T.img.path23 <- lapply(list.files(path=("./Calibration/Cal_pics/2017_04_08/2/"), (pattern="0141.*.tif$"), 
+                                  full.names=T), raster) #RP02-...282 ->50%
+
+T.img.path23 <- lapply(list.files(path=("./Calibration/Cal_pics/2017_04_08/2/"), (pattern="0142.*.tif$"), 
+                                  full.names=T), raster) #RP02-...200 ->80%
+
+T.img.path23 <- lapply(list.files(path=("./Calibration/Cal_pics/2017_04_08/2/"), (pattern="0143.*.tif$"), 
+                                  full.names=T), raster) #44%
+
+T.img.path23 <- lapply(list.files(path=("./Calibration/Cal_pics/2017_04_08/2/"), (pattern="0144.*.tif$"), 
+                                  full.names=T), raster) #44%
+
+T.img.path23 <- lapply(list.files(path=("./Calibration/Cal_pics/2017_04_08/2/"), (pattern="0145.*.tif$"), 
+                                  full.names=T), raster) #23%
+
+T.img.path23 <- lapply(list.files(path=("./Calibration/Cal_pics/2017_04_08/2/"), (pattern="0146.*.tif$"), 
+                                  full.names=T), raster) #23%
+
+
+T.img1 <- stack(T.img.path)
+beginCluster()#to speed up the process
+plotRGB(T.img1, r=3, g=2, b=1, 65535, colNA='white')  #PlotRGB
+endCluster()
+
+# PLot Red Edge band 
+plot(T.img1@layers[[5]])  
+
+#Select target extent
+cat("Click two points to define the target\n")
+Ext.1 <- drawExtent()
+
+#Extract values
+cat("Extracting target pixels\n")
+Mica.v1 <- extract(T.img1, Ext.1, df=T)
+
+
+#Calculate mean pixel values under each target polygon for each image band
+Mica.L <- melt(Mica.v, variable.name="Band", value.name = "DN") #Puts all the values in one column
+head(Mica.L)
