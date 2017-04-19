04bb0d53fcfe53b7c852ca5fd1b50f5e3a628c38
Ximena Tagle Casapía <xtagle@lamolina.edu.pe>
2017-04-19 23:01:06 +0200

    Update 2.Linear reg_Wang.R

--- 2.Linear reg_Wang.R
+++ 2.Linear reg_Wang.R
@@ -12,9 +12,11 @@ library(maptools)
 library(stringr)
 library(Rmisc)
 library(mapview)
+library(cowplot)
 
 #Load functions
 source("./lib/load.file.R")
+source("./lib/plot.replicates.R")
 
 #Load data
 dir(,"csv")
@@ -67,7 +69,15 @@ summary(T80$X80)
 ##Combine all the df into one
 RefT<-rbind(T9,T23,T44,T50,T80)
 
+summary(RefT)
 
+write.csv(RefT,"RefTargets.csv")
+
+#Plot Gray target reflectance vs DNs
+plot(Reflectance~DN, RefT)
+
+
+  
 #Split per band
 B<-RefT[RefT$Band==1,]
 G<-RefT[RefT$Band==2,]
@@ -75,35 +85,78 @@ R<-RefT[RefT$Band==3,]
 NIR<-RefT[RefT$Band==4,]
 Re<-RefT[RefT$Band==5,]
   
-  
-#Plot Gray target reflectance vs DNs
 
-
-
-
-
-
-##Regression x=DN, y=mean reflectance targets
+##Regression x=DN, y=mean reflectance targets https://www.otexts.org/fpp/4/4
 
 
 #Linear regression
 #Band 1
-regr.l1 <-lm(RefConvol~DN, B)
-regr.l1
-summary(regr.l1)
+LR.1 <-lm(Reflectance~DN, B)
+LR.1
+summary(LR.1)
 
 dev.off()
-plot(RefConvol~DN, Merge.M)
-abline(regr.l) 
-predict(regr.l)
+plot(Reflectance~DN, B)  
+abline(LR.1) 
+
+#Band 2
+LR.2 <-lm(Reflectance~DN, B)
+LR.2
+summary(LR.2)
+
+#Band 3
+LR.3 <-lm(Reflectance~DN, B)
+LR.3
+summary(LR.3)
+
+#Band 4
+LR.4 <-lm(Reflectance~DN, B)
+LR.4
+summary(LR.4)
+
+#Band 5
+LR.5 <-lm(Reflectance~DN, B)
+LR.5
+summary(LR.5)
 
 
-#Log
-regr.log <- glm(RefConvol~DN, Merge.M, family="binomial")
+
+dev.off()
+plot(Reflectance~DN, B)  
+abline(LR.1) 
+
+plot(Reflectance~DN, G)
+abline(LR.2) 
+
+plot(Reflectance~DN, R)  
+abline(LR.3) 
+
+plot(Reflectance~DN, NIR)  
+abline(LR.4)
+
+plot(Reflectance~DN, Re)  
+abline(LR.5)
+
+
+
+predict(LR.1)
 
 
 #Exponential
-regr.e <- nls(RefConvol ~ exp(a + b * x), data = B, start = list(a = 0, b = 0))
+Er.1 <- nls(RefConvol ~ exp(a + b * x), data = B, start = list(a = 0, b = 0))
 
 
 ##Natural logarithm of the reflectance
+
+
+
+
+
+
+ggplot(RefT, aes(x = DN, y = Reflectance, colour=Band)) + 
+  geom_point()+
+  scale_y_continuous(expand = c(0, -0.05), limits = c(-0.05, 1.05))+            #Set max 1
+  theme(axis.text=element_text(size=12),
+        axis.title=element_text(size=14,face="bold"))+
+  xlab("Digital Number (DN)") + ylab("Reflectance") +
+  background_grid(major = "none", minor = "none")
