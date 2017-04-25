6dbb07a9baa593d3d6d2e606e5e386ec955f6f8d
Ximena Tagle Casapía <xtagle@lamolina.edu.pe>
2017-04-25 16:14:03 +0200

    Script

--- 1.Transfer EXIF.R
+++ 1.Transfer EXIF.R
@@ -1,4 +1,3 @@
-
 #Set WD
 #setwd("H:/Ximena/MSc Thesis/Results/UAV imagery/")
 setwd("C:/Users/Ximena/Google Drive/MSc Thesis/Results/UAV imagery/")
@@ -10,7 +9,7 @@ library(exifr) #On windows it is needed to install strawberry http://strawberryp
 #Load imgs
 dir(,"tif")
 T.img.O <- list.files(path=("./Lonnstorp/2017_04_08/Micasense/Flight_2/000"), (pattern=".tif$"), 
-                         full.names=F) #Load all images
+                      full.names=F) #Load all images
 
 T.img.H <- list.files(path=("./Lonnstorp/2017_04_08/Micasense/Flight_2/H"), (pattern=".tif$"), 
                       full.names=F) #Load all images
@@ -18,23 +17,28 @@ T.img.H <- list.files(path=("./Lonnstorp/2017_04_08/Micasense/Flight_2/H"), (pat
 
 
 #Transfer EXIF information from original images to converted images
-exiftool.call("--help")
 
-exiftool.call("-tagsfromfile C:/Users/Ximena/Lonnstorp/2017_04_08/Micasense/Flight_2/000/IMG_0000_1.tif -ext TIF -all:all -makernotes -make -model -P C:/Users/Ximena/Lonnstorp/2017_04_08/Micasense/Flight_2/H/IMG_0000_1.tif")
+#exiftool.call("--help")
+
+#One img
+#exiftool.call("-tagsfromfile C:/Users/Ximena/Lonnstorp/2017_04_08/Micasense/Flight_2/000/IMG_0000_1.tif -ext TIF -all:all -makernotes -make -model -P C:/Users/Ximena/Lonnstorp/2017_04_08/Micasense/Flight_2/H/IMG_0000_1.tif")
 
 
-for(i in 1:length(T.img.H))
-              {
-              T.img.H[i] <- (paste0("-tagsfromfile C:/Users/Ximena/Lonnstorp/2017_04_08/Micasense/Flight_2/000/",T.img.O[i],
-              " -all:all -makernotes -make -model -P C:/Users/Ximena/Lonnstorp/2017_04_08/Micasense/Flight_2/H/IMG_0000_",
-              i, ".tif"))
-
-              
+#Multiple imgs
+for(i in 1:length(T.img.H)) #Create the command
+{
+  T.img.H[i] <- (paste0("-tagsfromfile C:/Users/Ximena/Lonnstorp/2017_04_08/Micasense/Flight_2/000/",T.img.O[i],
+                        " -all:all -makernotes -make -model -P -overwrite_original C:/Users/Ximena/Lonnstorp/2017_04_08/Micasense/Flight_2/H/",T.img.O[i]))
+  
 }
 
 
+#Copy the exif to the new imgs
 for(i in 1:length(T.img.H))
 {
-exiftool.call(T.img.H[i])              
+  exiftool.call(T.img.H[i])              
 }
 
+
+#End
+######
