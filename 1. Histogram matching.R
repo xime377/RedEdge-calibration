0e38de95853df435adf65d1dfd3d1c128dae2dda
Ximena Tagle Casapía <xtagle@lamolina.edu.pe>
2017-04-25 11:56:22 +0200

    Script

--- 1. Histogram matching.R
+++ 1. Histogram matching.R
@@ -1,6 +1,6 @@
 #Set WD
-#setwd("E:/Ximena/MSc Thesis/Results/UAV imagery/")
-setwd("C:/Users/Ximena/Google Drive/MSc Thesis/Results/UAV imagery/")
+setwd("H:/Ximena/MSc Thesis/Results/UAV imagery/")
+#setwd("C:/Users/Ximena/Google Drive/MSc Thesis/Results/UAV imagery/")
 
 ###Load libraries
 library(raster)
@@ -14,14 +14,14 @@ T.img.path1 <- list.files(path=("./Lonnstorp/2017_04_08/Micasense/Flight_2/000")
                                 full.names=T)
 
 T.img1<- lapply(T.img.path1,raster) #Load all bands
-names(T.img1)<-T.img.path
+names(T.img1)<-T.img.path1
 
 #Band2
 T.img.path2 <- list.files(path=("./Lonnstorp/2017_04_08/Micasense/Flight_2/000"), pattern="_2.*.tif$", 
                           full.names=T)
 
 T.img2<- lapply(T.img.path2,raster) #Load all bands
-names(T.img2)<-T.img.path
+names(T.img2)<-T.img.path2
 
 
 #Band3
@@ -29,21 +29,21 @@ T.img.path3 <- list.files(path=("./Lonnstorp/2017_04_08/Micasense/Flight_2/000")
                           full.names=T)
 
 T.img3<- lapply(T.img.path3,raster) #Load all bands
-names(T.img3)<-T.img.path
+names(T.img3)<-T.img.path3
 
 #Band4
 T.img.path4 <- list.files(path=("./Lonnstorp/2017_04_08/Micasense/Flight_2/000"), pattern="_4.*.tif$", 
                           full.names=T)
 
 T.img4<- lapply(T.img.path4,raster) #Load all bands
-names(T.img4)<-T.img.path
+names(T.img4)<-T.img.path4
 
 #Band5
 T.img.path5 <- list.files(path=("./Lonnstorp/2017_04_08/Micasense/Flight_2/000"), pattern="_5.*.tif$", 
                           full.names=T)
 
 T.img5<- lapply(T.img.path5,raster) #Load all bands
-names(T.img5)<-T.img.path
+names(T.img5)<-T.img.path5
 
 
 ###
@@ -52,7 +52,8 @@ for (i in 1:length(T.img1))
 {
   T.img1[[i]] <- histMatch(T.img1[[i]],T.img1[[32]])   #histogram matching imgage 31
   writeRaster(T.img1[[i]],paste0("./Lonnstorp/2017_04_08/Micasense/Flight_2/H/",
-                                substr(names(T.img1[i]),47,56),".tif"),datatype="FLT4S")
+                                substr(names(T.img1[i]),47,56),".tif"),datatype="INT2U",
+              options="COMPRESS=NONE")
 }
 
 
@@ -78,20 +79,45 @@ compareRaster(imgH,img2)
 
 for (i in 1:length(T.img2))   
 {
-  T.img2[[i]] <- histMatch(T.img2[[i]],T.img2[[32]])   #histogram matching imgage 32
-  writeRaster(T.img2[[i]],paste0("./Lonnstorp/2027_04_08/Micasense/Flight_2/H/",
-                                 substr(names(T.img2[i]),47,56),".tif"),datatype="FLT4S")
+  T.img2[[i]] <- histMatch(T.img2[[i]],T.img2[[32]])   #histogram matching imgage 31
+  writeRaster(T.img2[[i]],paste0("./Lonnstorp/2017_04_08/Micasense/Flight_2/H/",
+                                 substr(names(T.img2[i]),47,56),".tif"),datatype="INT2U",
+              options="COMPRESS=NONE")
 }
+
 
 #################################################################################################
 
 #Matching histogram for band 3
 
-for (i in 1:length(T.img2))   
+for (i in 1:length(T.img3))   
 {
-  T.img2[[i]] <- histMatch(T.img2[[i]],T.img2[[32]])   #histogram matching imgage 32
-  writeRaster(T.img2[[i]],paste0("./Lonnstorp/2027_04_08/Micasense/Flight_2/H/",
-                                 substr(names(T.img2[i]),47,56),".tif"),datatype="FLT4S")
+  T.img3[[i]] <- histMatch(T.img3[[i]],T.img3[[32]])   #histogram matching imgage 31
+  writeRaster(T.img3[[i]],paste0("./Lonnstorp/2021_04_08/Micasense/Flight_2/H/",
+                                substr(names(T.img3[i]),47,56),".tif"),datatype="INT2U",
+  options="COMPRESS=NONE")
 }
 
 #################################################################################################
+
+#Matching histogram for band 4
+
+for (i in 1:length(T.img4))   
+{
+  T.img4[[i]] <- histMatch(T.img4[[i]],T.img4[[32]])   #histogram matching imgage 31
+  writeRaster(T.img4[[i]],paste0("./Lonnstorp/2021_04_08/Micasense/Flight_2/H/",
+                                 substr(names(T.img4[i]),47,56),".tif"),datatype="INT2U",
+              options="COMPRESS=NONE")
+}
+
+#################################################################################################
+
+#Matching histogram for band 5
+
+for (i in 1:length(T.img5))   
+{
+  T.img5[[i]] <- histMatch(T.img5[[i]],T.img5[[32]])   #histogram matching imgage 31
+  writeRaster(T.img5[[i]],paste0("./Lonnstorp/2021_04_08/Micasense/Flight_2/H/",
+                                 substr(names(T.img5[i]),47,56),".tif"),datatype="INT2U",
+              options="COMPRESS=NONE")
+}
