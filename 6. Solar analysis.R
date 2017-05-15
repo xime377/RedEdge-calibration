0932fa3a1cebb2cdccb2beffeebe688c7309105b
Ximena Tagle Casapía <xtagle@lamolina.edu.pe>
2017-05-15 02:38:54 +0200

    Script

--- 6. Solar analysis.R
+++ 6. Solar analysis.R
@@ -14,7 +14,7 @@ head(Exif)
 
 
 #Load images
-T.img.path<- list.files(path=("./UAV imagery/Lonnstorp/2017_04_08/Micasense/Flight_2/EC"), (pattern=".tif$"), 
+T.img.path<- list.files(path=("./UAV imagery/Lonnstorp/2017_04_08/Micasense/Flight_2/000"), (pattern=".tif$"), 
                         full.names=T) #List images
 
 T.img<- lapply(T.img.path,raster) #Load all bands
@@ -25,9 +25,9 @@ names(T.img)<-T.img.path
 
 for (i in 1:length(T.img))   
 {
-  T.img[[i]]<-(T.img[[i]]* (sin(Exif$SunA[i]*pi/180))
-  writeRaster(T.img[[i]],paste0("./UAV imagery/Lonnstorp/2017_04_08/Micasense/Flight_2/IM_EC/",
-                                substr(names(T.img[i]),58,67),".tif"), datatype="INT2U",
+  T.img[[i]]<-T.img[[i]]* (sin(Exif$SunA[i]*pi/180))
+  writeRaster(T.img[[i]],paste0("./UAV imagery/Lonnstorp/2017_04_08/Micasense/Flight_2/SEA/",
+                                substr(names(T.img[i]),59,68),".tif"), datatype="INT2U",
               options="COMPRESS=NONE", overwrite=T)  
 }
 
