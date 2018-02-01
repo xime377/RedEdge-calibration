35ac497f0ad51404a2adff8690a86f74deac2abc
Ximena Tagle Casapía <xtagle@iiap.org.pe>
2018-02-01 13:53:02 +0100

    1. Cal target.R
    Script

--- 1.Cal target.R
+++ 1.Cal target.R
@@ -1,4 +1,4 @@
-###Calculates statistics of the reflectance targets##
+###Calculates statistics of the reflectance targets. It needs the spectral measurements of them##
 #Set WD
 setwd("H:/Ximena/MSc Thesis/Results")
 #setwd("D:/Google Drive/MSc Thesis/Results")
